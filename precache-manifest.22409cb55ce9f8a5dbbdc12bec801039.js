657f685e35b2d3b792a763852acab4b1dc8f8401
Pooja <pooja.20b0103007@abes.ac.in>
2022-03-07 00:56:31 +0530

    Updates

---
+++ precache-manifest.22409cb55ce9f8a5dbbdc12bec801039.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e1c48b833275a277e74",
+    "url": "/reactTravelProject/static/css/main.eff4e1c9.chunk.css"
+  },
+  {
+    "revision": "5e1c48b833275a277e74",
+    "url": "/reactTravelProject/static/js/main.5e1c48b8.chunk.js"
+  },
+  {
+    "revision": "f7096f4ada9a9f6eb5b2",
+    "url": "/reactTravelProject/static/js/1.f7096f4a.chunk.js"
+  },
+  {
+    "revision": "3c880ecb9a9a0738cf8b",
+    "url": "/reactTravelProject/static/js/runtime~main.3c880ecb.js"
+  },
+  {
+    "revision": "e1f4326f7e6a9f9134ba747a2b526b65",
+    "url": "/reactTravelProject/static/media/img-8.e1f4326f.jpg"
+  },
+  {
+    "revision": "36d5a87c511bab9639ad7750ffd9052c",
+    "url": "/reactTravelProject/static/media/img-1.36d5a87c.jpg"
+  },
+  {
+    "revision": "3df2bb986be6e43d8efda327b7e5c524",
+    "url": "/reactTravelProject/static/media/img-2.3df2bb98.jpg"
+  },
+  {
+    "revision": "314da2d362bd18f6d13101c5732d8a0d",
+    "url": "/reactTravelProject/static/media/img-9.314da2d3.jpg"
+  },
+  {
+    "revision": "64272f77efb5cee2cbb55e9314f04904",
+    "url": "/reactTravelProject/static/media/img-4.64272f77.jpg"
+  },
+  {
+    "revision": "674ce46e7138eab0f7c7e2dbe9792d70",
+    "url": "/reactTravelProject/static/media/img-3.674ce46e.jpg"
+  },
+  {
+    "revision": "faad3897e430f8cc6cad540fe6b3674a",
+    "url": "/reactTravelProject/static/media/img-home.faad3897.jpg"
+  },
+  {
+    "revision": "3dcc291e6c6aee8ab9c7235435b80561",
+    "url": "/reactTravelProject/index.html"
+  }
+];
